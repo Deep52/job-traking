0283326f69db2db10d95659a9a67788ab7231545
Deep52 <pardeep.nsr@gmail.com>
2022-06-17 14:16:56 +0100

    app

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # job-traking
+# Installation
+
+To run the code please install all the required dependencies by running:
+`npm install`
+
+A local database is required to be created using postgresql. Run job-tracking.sql in your local database to create the required tables.
+
+create a file named config.js in the root directory in the following format:
--- app.js
+++ app.js
@@ -6,13 +6,38 @@ const PORT = 5000;
 const path = require("path");
 const config = require("./config.js")[env];
 const Pool = require("pg").Pool;
+const bodyParser = require("body-parser");
 
+
+
+
+//set view engine to use ejs templates
+app.set("view engine", "ejs");
+
+// parse application/x-www-form-urlencoded
+app.use(express.json())
+app.use(express.urlencoded());
+
+//use router for articles   
+// app.use("/articles", articleRouter);
+
+// static file directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//app.use(express.urlencoded());
+
+
+
+//route for index page
 app.get("/", function(req, res) {
-
-
-    res.redirect("/");
-
-})
+    res.render("index");
+});
 app.listen(PORT, () => {
     console.log(`Server running on port: http://localhost:${PORT}`);
 });
