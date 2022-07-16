9fba800eeec02ed77c421a7ad73f290b223fee8c
Deep52 <pardeep.nsr@gmail.com>
2022-07-16 18:19:48 +0100

    app

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const config = require("./config.js")[env];
 const Pool = require("pg").Pool;
 const bodyParser = require("body-parser");
 const { count } = require("console");
+const { createHash, scryptSync, randomBytes } = require('crypto');
 
 
 
@@ -65,7 +66,10 @@ app.get("/student-dashboard", function(req, res) {
 app.get("/add-job", function(req, res) {
     res.render("add-job");
 });
-
+// create hashing function
+function hash(input) {
+    return createHash('sha256').update(input).digest('hex');
+}
 
 
 
@@ -74,8 +78,43 @@ app.get("/add-job", function(req, res) {
 // register user function
 
 app.post("/register", async(req, res, next) => {
+    const pool = new Pool(config);
+    const client = await pool.connect();
+    const { firstName, lastName, email, Department, course, confirm_password, type } = req.body;
+    const salt = randomBytes(16).toString('hex');
+    const hashedPassword = hash(confirm_password + salt);
+    // current date
+    // adjust 0 before single digit date
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+
+    // current month
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+    // current year
+    let year = date_ob.getFullYear();
+
+    const start_date = year + "-" + month + "-" + date;
+    if (req.body.type == 'Student') {
+        var type_access = '3';
+    }
+    if (req.body.type == 'Professor') {
+        var type_access = '2';
+    } else {
+        var type_access = '1';
+    }
+    console.log(type);
+
+    //const date = date();
+    // const type_access = '';
+
+    const result = await client.query("INSERT INTO public.login (fname, lname, email,password, salt,access,type, start-date) VALUES ($1, $2, $3, $4, $5,$6,$7,$8) RETURNING *", [firstName, lastName, email, hashedPassword, salt, type_access, type, start_date]).then(results_insert => {
+
+
+
+    });
     // const { firstName, lastName, email, password, course, type } = req.body;
-    console.log(req.body);
+    // console.log(req.body.firstName);
 
 });
 app.post("/dept", async(req, res, next) => {
